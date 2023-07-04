3c8996e65a6570c6070562aed51872d254562c9f
minnim1010 <pms1010123@daum.net>
2023-07-04 23:19:50 +0900

    INIT: Make backend base

---
+++ backend/src/test/java/spring/bbs/BbsApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.bbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
