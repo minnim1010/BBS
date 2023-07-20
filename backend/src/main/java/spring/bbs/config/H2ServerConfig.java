e243a4e70088e0ed9a7dfca11f9429da9c744a29
minnim1010 <pms1010123@daum.net>
2023-07-20 22:55:04 +0900

    FEAT(config): Add H2 server mode config

---
+++ backend/src/main/java/spring/bbs/config/H2ServerConfig.java
@@ -0,0 +1,15 @@
+package spring.bbs.config;
+
+import org.h2.tools.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.SQLException;
+
+@Configuration
+public class H2ServerConfig {
+    @Bean
+    public Server H2DatabaseServer() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9091");
+    }
+}
