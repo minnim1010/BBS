070e3e0a0d91fc6600e1013f08ca9f1a9492de7b
minnim1010 <pms1010123@daum.net>
2023-08-16 19:37:37 +0900

    FIX(CI): 프로필에 따라 h2 console config 설정하도록 변경

--- backend/src/main/java/spring/bbs/config/security/SecurityConfig.java
+++ backend/src/main/java/spring/bbs/config/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package spring.bbs.config.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
